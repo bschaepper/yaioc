19d09184075da5ca55d8e27539daf637cba6740e
Bruno Schäpper <b.schaepper@hotmail.ch>
2017-11-20 17:11:05 +0100

    splitted tests

--- test/RegisterAndResolveTests.js
+++ test/RegisterAndResolveTests.js
@@ -9,7 +9,7 @@ const TargetFunction = require("./TestMocks").TargetFunction;
 const TargetClass = require("./TestMocks").TargetClass;
 
 
-describe("yaioc test", () => {
+describe("Register and Resolve Tests", () => {
 
     let container;
 
@@ -17,7 +17,7 @@ describe("yaioc test", () => {
         container = yaioc.container();
     });
 
-    describe("register and resolve", () => {
+    describe("register and resolve value objects", () => {
 
         it("should return simple value objects", () => {
             const dependencyOne = {};
@@ -36,6 +36,10 @@ describe("yaioc test", () => {
 
             expect(resolved).to.be.equal(dependencyOne);
         });
+
+    });
+
+    describe("register and resolve classes and dependencies", () => {
 
         it("should instantiate object with dependencies", () => {
             const dependencyOne = {};
@@ -136,93 +140,6 @@ describe("yaioc test", () => {
             const targetInstance = container.get("target");
 
             expect(targetInstance.dependency).to.be.instanceof(Dependency);
-        });
-
-    });
-
-    describe("errors", () => {
-
-        it("should throw if dependency cannot be resolved", () => {
-            const container = yaioc.container();
-            container.register(TargetFunction);
-
-            const action = container.get.bind(container, "targetFunction");
-
-            expect(action).to.throw(/Could not satisfy dependency/);
-        });
-
-        it("should include name of missing dependency", () => {
-            const container = yaioc.container();
-            container.register(TargetFunction);
-
-            const action = container.get.bind(container, "targetFunction");
-
-            expect(action).to.throw(/dependencyOne/);
-        });
-
-        it("should include traget name of missing dependency", () => {
-            const container = yaioc.container();
-            container.register(TargetFunction);
-
-            const action = container.get.bind(container, "targetFunction");
-
-            expect(action).to.throw(/TargetFunction/i);
-        });
-
-        it("should throw if no name is present and cannot be resolved", () => {
-
-            const callRegister = container.register.bind(container, {});
-
-            expect(callRegister).to.throw(/no name provided for dependency/);
-        });
-
-    });
-
-    describe("cache", () => {
-
-        it("should resolve to new instance on every call", () => {
-            container.register("dependencyOne", {});
-            container.register("dependencyTwo", {});
-            container.register(TargetFunction);
-
-            const instanceA = container.get("targetFunction");
-            const instanceB = container.get("targetFunction");
-
-            expect(instanceA).to.be.instanceof(TargetFunction);
-            expect(instanceB).to.be.instanceof(TargetFunction);
-            expect(instanceA === instanceB).to.be.eql(false);
-        });
-
-        it("should resolve to same instance on every call, when registered in cache", () => {
-            container.register("dependencyOne", {});
-            container.register("dependencyTwo", {});
-            container.cache().register(TargetFunction);
-
-            const instanceA = container.get("targetFunction");
-            const instanceB = container.get("targetFunction");
-
-            expect(instanceA).to.be.instanceof(TargetFunction);
-            expect(instanceB).to.be.instanceof(TargetFunction);
-            expect(instanceA === instanceB).to.be.eql(true);
-        });
-
-        it("should forgive subsequent cache() calls and return same cache", () => {
-            const cachedContainer1 = container.cache();
-
-            const cachedContainer2 = cachedContainer1.cache();
-
-            expect(cachedContainer1 === cachedContainer2).to.be.eql(true);
-        });
-
-        it("should allow short form of adaptor to be cached", () => {
-            container.cache().registerAdaptor("targetAdaptor", () => {
-                return {};
-            });
-
-            const instanceA = container.get("targetAdaptor");
-            const instanceB = container.get("targetAdaptor");
-
-            expect(instanceA === instanceB).to.be.eql(true);
         });
 
     });
@@ -334,88 +251,6 @@ describe("yaioc test", () => {
             container.get("adaptorThing");
 
             expect(actualInto).to.be.eql(void 0);
-        });
-
-    });
-
-    describe("ease of use", () => {
-
-        it("yaioc() should return a Container instance", () => {
-            const container = yaioc();
-
-            expect(container).to.be.instanceof(yaioc.Container);
-        });
-
-        it("yaioc() should return always the same Container instance", () => {
-            const container1 = yaioc();
-            const container2 = yaioc();
-
-            expect(container1 === container2).to.be.eql(true);
-        });
-
-    });
-
-    describe("child containers", () => {
-
-        it("should optionally take a container in constructor, which is used for dependency resolving", () => {
-            const wrappedContainer = yaioc.container();
-            const container = yaioc.container(wrappedContainer);
-
-            const foo = {};
-            wrappedContainer.register("foo", foo);
-
-            const resolvedFoo = container.get("foo");
-
-            expect(resolvedFoo === foo).to.be.eql(true);
-        });
-
-        it("should optionally take an array of child containers", () => {
-            const childContainers = [yaioc.container(), yaioc.container()];
-            const container = yaioc.container(childContainers);
-
-            const foos = [{}, {}];
-            childContainers[0].register("foo0", foos[0]);
-            childContainers[1].register("foo1", foos[1]);
-
-            const resolvedFoos = [container.get("foo0"), container.get("foo1")];
-
-            expect(resolvedFoos[0] === foos[0]).to.be.eql(true);
-            expect(resolvedFoos[1] === foos[1]).to.be.eql(true);
-        });
-
-        it("should not resolve dependencies in outer container", () => {
-            const wrappedContainer = yaioc.container();
-            const container = yaioc.container(wrappedContainer);
-
-            const foo = {};
-            container.register("foo", foo);
-
-            const resolvedFoo = wrappedContainer.get("foo");
-
-            expect(resolvedFoo).to.be.eql(void 0);
-        });
-
-        it("should resolve constructors in wrapped container", () => {
-            const wrappedContainer = yaioc.container();
-            const container = yaioc.container(wrappedContainer);
-            wrappedContainer.register(TargetFunction);
-            wrappedContainer.register("dependencyOne", {});
-            wrappedContainer.register("dependencyTwo", {});
-
-            const targetFunction = container.get("targetFunction");
-
-            expect(targetFunction).to.be.a.instanceof(TargetFunction);
-        });
-
-    });
-
-    describe("component scan", () => {
-
-        it("should match components and add to container", () => {
-
-            container.scanComponents(__dirname + "/**/*Mocks.js");
-
-            expect(container.lookup("TestMocks")).to.be.an("object");
         });
 
     });
